089a51dd33d715f39adf58b6b7fe3c43813038fb
pobo380 <n3560k@gmail.com>
2013-12-03 05:27:43 +0900

    CLIインターフェースを定義した.

--- lib/eccube/plugin.rb
+++ lib/eccube/plugin.rb
@@ -1,7 +1,34 @@
 require "eccube/plugin/version"
+require "thor"
 
 module Eccube
   module Plugin
-    # Your code goes here...
+    module CLI
+      class Hooks < Thor
+        desc 'add [target]', 'generate hook class file'
+        def add(target)
+        end
+
+        desc 'list', 'print list of hookable classes'
+        def list(target)
+        end
+      end
+
+      class Main < Thor
+        desc 'new [plugin_code]', 'create a new plugin from skeleton'
+        def new(plugin_code)
+        end
+
+        desc 'deploy [host]', 'deploy plugin to target host'
+        def deploy(host=:all)
+        end
+
+        desc 'package [filepath]', 'package plugin to PLUGINCODE.tar.gz'
+        def package(filepath=nil)
+        end
+
+        register Hooks, 'hooks', 'hooks [COMMAND]', 'commands related to EC-CUBE hook points'
+      end
+    end
   end
 end
--- spec/eccube/plugin_spec.rb
+++ spec/eccube/plugin_spec.rb
@@ -5,7 +5,23 @@ describe Eccube::Plugin do
     Eccube::Plugin::VERSION.should_not be_nil
   end
 
-  it 'should do something useful' do
-    false.should be_true
+  describe Eccube::Plugin::CLI do
+    subject { Eccube::Plugin::CLI::Main.new }
+
+    [:new, :'hooks', :help, :deploy, :package].each do |key|
+      it "should have a #{key} command" do
+        expect(subject).to respond_to(key)
+      end
+    end
+
+    describe Eccube::Plugin::CLI::Hooks do
+      subject { Eccube::Plugin::CLI::Hooks.new }
+
+      [:add, :list].each do |key|
+        it "should have a #{key} command" do
+          expect(subject).to respond_to(key)
+        end
+      end
+    end
   end
 end

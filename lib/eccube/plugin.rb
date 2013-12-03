32809c728798eba4f94af9e7297dd33a42d11d38
pobo380 <n3560k@gmail.com>
2013-12-04 02:47:23 +0900

    スケルトン生成するとこまでできた.
    Thor::Action::directoryマジ便利.
    
      * テンプレートに値を埋め込んで生成 (*.tt)
      * 空ディレクトリを生成 (.empty_directory)
      * ファイル名を変更して生成 (%var%.*)
    
    が簡単にできた.

--- lib/eccube/plugin.rb
+++ lib/eccube/plugin.rb
@@ -1,4 +1,7 @@
 require "eccube/plugin/version"
+require "eccube/plugin/license"
+require "yaml"
+require "date"
 require "thor"
 
 module Eccube
@@ -15,8 +18,49 @@ module Eccube
       end
 
       class Main < Thor
-        desc 'new [plugin_code]', 'create a new plugin from skeleton'
+        include Thor::Actions
+
+        class << self
+          def source_root
+            File.expand_path(File.dirname(__FILE__))
+          end
+        end
+
+        attr_accessor :plugin_code
+
+        desc 'new [plugin_code]', 'generate a new plugin from skeleton'
         def new(plugin_code)
+          self.plugin_code = plugin_code
+
+          ## Input plugin informations.
+          plugin_info = { :plugin_code => plugin_code, :year => Date.today.year }
+
+          say set_color('Generate a new plugin from skeleton.', :cyan)
+          plugin_info[:author] = ask 'Input author: '
+
+          licenses_table = License::Licenses.map.with_index {|license, idx| [idx, license[:name]]}
+          say set_color('Choose your software license. Reference: http://opensource.org/licenses', :cyan)
+          begin
+            print_table licenses_table
+            license_idx = ask('> ').to_i
+
+            plugin_info[:license]     = License::Licenses[license_idx][:name]
+            plugin_info[:license_url] = License::Licenses[license_idx][:url]
+          rescue
+            say set_color('Invalid number. Choose your software license.', :red)
+            retry
+          end
+
+          ## Generate plugin directory and .eccube-plugin.yml
+          begin
+            raise Errno::EEXIST if File.exists?(File.join(Dir.pwd, plugin_code))
+
+            directory('templates', plugin_code, plugin_info)
+
+            say set_color("Initialization Complete!", :green)
+          rescue Errno::EEXIST => e
+            say set_color("Failed: '#{plugin_code}' directory already exists.", :red)
+          end
         end
 
         desc 'deploy [host]', 'deploy plugin to target host'
---
+++ lib/eccube/plugin/license.rb
@@ -0,0 +1,19 @@
+module Eccube
+  module Plugin
+    class License
+      Licenses = [
+        {:name => 'Apache-2.0',   :url => 'http://opensource.org/licenses/Apache-2.0'   },
+        {:name => 'BSD 3-Clause', :url => 'http://opensource.org/licenses/BSD-3-Clause' },
+        {:name => 'BSD 2-Clause', :url => 'http://opensource.org/licenses/BSD-2-Clause' },
+        {:name => 'GPL-2.0',      :url => 'http://opensource.org/licenses/GPL-2.0'      },
+        {:name => 'GPL-3.0',      :url => 'http://opensource.org/licenses/GPL-3.0'      },
+        {:name => 'LGPL-2.1',     :url => 'http://opensource.org/licenses/LGPL-2.1'     },
+        {:name => 'LGPL-3.0',     :url => 'http://opensource.org/licenses/LGPL-3.0'     },
+        {:name => 'MIT',          :url => 'http://opensource.org/licenses/MIT'          },
+        {:name => 'MPL-2.0',      :url => 'http://opensource.org/licenses/MPL-2.0'      },
+        {:name => 'CDDL-1.0',     :url => 'http://opensource.org/licenses/CDDL-1.0'     },
+        {:name => 'EPL-1.0',      :url => 'http://opensource.org/licenses/EPL-1.0'      },
+      ]
+    end
+  end
+end
